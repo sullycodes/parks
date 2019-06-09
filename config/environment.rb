3a1c75f30bc71008c288da8c159e902bedc3b973
unknown <dansullivan19@gmail.com>
2019-06-09 14:09:16 -0400

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,12 @@
+# require 'bundler'
+# Bundler.require
+
+# require_all 'lib' #not working "undefined require_all"
+
+require 'nokogiri'
+require 'open-uri'
+
+# require_relative '../lib/scraper'
+require '../lib/parks'
+
+
